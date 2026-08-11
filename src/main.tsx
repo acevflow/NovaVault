fa8d835d5ba4972d6873477f9464c315b162ce69
Acev Flow <acevflow@gmail.com>
2026-08-11 23:35:51 +0200

    refactor: move animations to dedicated stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/fonts.css";
 import "./styles/theme.css";
+import "./styles/fonts.css";
+import "./styles/animations.css";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
